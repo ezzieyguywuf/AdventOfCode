1c602fe5b303c18e8b2ecaaf4607d1be71a5505f
ezzieyguywuf <WolfgangESanyer@gmail.com>
2020-12-04 20:24:53 -0500

    Added a module for day1

---
+++ 2020/Day1.hs
@@ -0,0 +1,4 @@
+module Day1 (day1Main) where
+
+day1Main :: IO ()
+day1Main = putStrLn "Oh hai!"
--- 2020/Main.hs
+++ 2020/Main.hs
@@ -1,4 +1,6 @@
 module Main (main) where
 
+import Day1 (day1Main)
+
 main :: IO ()
-main = putStrLn "Hello AOC!"
+main = putStrLn "Hello AOC!" >> day1Main

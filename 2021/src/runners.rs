4d55b118e5637973ba9d58ea0b1398ac688cfcea
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-01 12:33:04 -0500

    Solution for day 01, problem 1
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners.rs
+++ 2021/src/runners.rs
@@ -2,10 +2,27 @@ use std::{fs, io};
 
 pub fn run_day_01() -> io::Result<()> {
   let file_data = fs::read_to_string("src/01_input.txt")?;
+  let mut lines = file_data.lines().map(|val| {
+    val
+      .parse::<i32>()
+      .expect(format!("Unable to parse value into integer: {}", val).as_str())
+  });
 
-  for line in file_data.lines() {
-    println!("{}", line);
+  let mut count = 0;
+  let mut prev = lines.next().expect("Unable to fetch next value");
+
+  for val in lines {
+    if val > prev {
+      count = count + 1;
+    }
+
+    prev = val;
   }
+
+  println!(
+    "There were {} measurements greater than the previous",
+    count
+  );
 
   Ok(())
 }

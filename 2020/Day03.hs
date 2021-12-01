dc5d8dd8cfb45f1a47f87d6b57ae53a8e801434c
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-01 09:09:10 -0500

    Moved 2020 stuff to be self-contained
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2020/Day03.hs
+++ 2020/Day03.hs
@@ -1,4 +1,42 @@
 module Day03 (runMain) where
 
+import Data.List (uncons)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as TextIO
+import Control.Monad.State (State, get, put, execState)
+
 runMain :: IO ()
-runMain = putStrLn "Can we do 3 in one day??!"
+runMain = do
+    putStrLn "Can we do 3 in one day??!"
+    rawLines <- take 4 . Text.lines <$> TextIO.readFile "2020/day03_input.txt" 
+
+    mapM_ TextIO.putStrLn rawLines
+    moveTobaggon rawLines
+
+type MyState = State Tobaggon
+
+data Tobaggon =
+    Tobaggon
+        { getCol   :: Int
+        , getRow   :: Int
+        , getTrees :: Int
+        } deriving (Show)
+
+
+-- Blahhhh State!!!!
+-- We can do this
+moveTobaggon :: [Text] -> MyState Bool
+moveTobaggon theMap = do
+    (Tobaggon col row trees) <- get
+    let newCol = if col' > rowLen then col' - rowLen  else col'
+        col' = col + 3
+        row' = row + 1
+        rowLen = Text.length (head theMap)
+        curRow = theMap !! row'
+    if row' > length theMap
+        then pure False
+        else do
+            let trees' = Text.index curRow newCol == '#'
+            put (Tobaggon newCol row' trees')
+            pure True

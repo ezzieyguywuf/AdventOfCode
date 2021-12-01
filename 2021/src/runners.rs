7b3dae35a72c36bbc26766a3fc574a64d48c9599
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-01 12:46:40 -0500

    Refactor: create file_to_ints helper function
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners.rs
+++ 2021/src/runners.rs
@@ -1,22 +1,33 @@
 use std::{fs, io};
 
-pub fn run_day_01() -> io::Result<()> {
-  let file_data = fs::read_to_string("src/01_input.txt")?;
-  let mut lines = file_data.lines().map(|val| {
+fn file_to_ints(fname: &str) -> io::Result<std::vec::Vec<i32>> {
+  let file_data = fs::read_to_string(fname)?;
+  let lines = file_data.lines().map(|val| {
     val
       .parse::<i32>()
       .expect(format!("Unable to parse value into integer: {}", val).as_str())
   });
-
-  let mut count = 0;
-  let mut prev = lines.next().expect("Unable to fetch next value");
+  let mut out = std::vec::Vec::new();
 
   for val in lines {
-    if val > prev {
+    out.push(val);
+  }
+
+  return Ok(out);
+}
+
+pub fn run_day_01() -> io::Result<()> {
+  let data = file_to_ints("src/01_input.txt")?;
+
+  let mut count = 0;
+  let mut prev = data[0];
+
+  for val in &data[1..] {
+    if *val > prev {
       count = count + 1;
     }
 
-    prev = val;
+    prev = *val;
   }
 
   println!(

4fd6ea8fa431a0f82cf4da53af54b610fc6f6e32
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-01 10:25:12 -0500

    Set up rustfmt
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners.rs
+++ 2021/src/runners.rs
@@ -1,12 +1,11 @@
-use std::{io, fs};
+use std::{fs, io};
 
 pub fn run_day_01() -> io::Result<()> {
-    let file_data = fs::read_to_string("src/01_input.txt")?;
+  let file_data = fs::read_to_string("src/01_input.txt")?;
 
-    for line in file_data.lines() {
-        println!("{}", line);
-    }
+  for line in file_data.lines() {
+    println!("{}", line);
+  }
 
-    Ok(())
+  Ok(())
 }
-

162895a42100c225db04367e9911e0a16d10a43d
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-05 13:15:30 -0500

    day04: parse numbers from input
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners/day04.rs
+++ 2021/src/runners/day04.rs
@@ -1,5 +1,24 @@
 use advent_of_code::util::*;
+pub use std::iter;
+
+fn parse_numbers<I: Iterator<Item = String>>(iter: I) -> Vec<u32> {
+  iter
+    .take_while(|elem| *elem != "")
+    .collect::<String>()
+    .split(',')
+    .map(|elem| {
+      elem
+        .parse::<u32>()
+        .unwrap_or_else(|_| panic!("Unable to parse '{}' into u32", elem))
+    })
+    .collect::<Vec<_>>()
+}
 
 pub fn run_a() {
-  println!("Hello, day04")
+  let lines = file_to_lines("data/04_input.txt");
+  let numbers = parse_numbers(lines);
+
+  for number in numbers {
+    println!("Got number: {}", number);
+  }
 }

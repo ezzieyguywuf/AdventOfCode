691d4f904e260cbff36449e26c89e7470df5ab4e
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-06 08:54:42 -0500

    day04: parse boards
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners/day04.rs
+++ 2021/src/runners/day04.rs
@@ -1,9 +1,35 @@
 use advent_of_code::util::*;
 pub use std::iter;
 
-fn parse_numbers<I: Iterator<Item = String>>(iter: I) -> Vec<u32> {
+#[derive(Debug)]
+struct Board {
+  data: Vec<u32>,
+}
+
+fn get_board(iter: &mut impl Iterator<Item = String>) -> Board {
+  let mut out: Board = Board { data: Vec::new() };
+
+  for line in iter {
+    if line.is_empty() {
+      return out;
+    } else {
+      let parsed = line.split_ascii_whitespace().map(|el| {
+        el.parse::<u32>()
+          .unwrap_or_else(|_| panic!("Unable to convert {} to u32. line = {}", el, line))
+      });
+
+      for item in parsed {
+        out.data.push(item);
+      }
+    }
+  }
+
+  out
+}
+
+fn parse_numbers(iter: &mut impl Iterator<Item = String>) -> Vec<u32> {
   iter
-    .take_while(|elem| *elem != "")
+    .take_while(|elem| !elem.is_empty())
     .collect::<String>()
     .split(',')
     .map(|elem| {
@@ -11,14 +37,22 @@ fn parse_numbers<I: Iterator<Item = String>>(iter: I) -> Vec<u32> {
         .parse::<u32>()
         .unwrap_or_else(|_| panic!("Unable to parse '{}' into u32", elem))
     })
-    .collect::<Vec<_>>()
+    .collect::<Vec<u32>>()
 }
 
 pub fn run_a() {
-  let lines = file_to_lines("data/04_input.txt");
-  let numbers = parse_numbers(lines);
+  let mut lines = file_to_lines("data/04_input.txt").peekable();
+  let numbers = parse_numbers(&mut lines);
 
-  for number in numbers {
+  for number in numbers.iter().take(4) {
     println!("Got number: {}", number);
   }
+
+  let mut boards: Vec<Board> = Vec::new();
+  while lines.peek() != None {
+    let board = get_board(&mut lines);
+    boards.push(board);
+  }
+
+  println!("Got boards: {:?}", boards);
 }

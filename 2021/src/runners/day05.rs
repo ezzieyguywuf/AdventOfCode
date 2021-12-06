5352ebc7618acf71fecd133f190dd7f153c492dc
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-06 16:48:35 -0500

    day05: solution for part a
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners/day05.rs
+++ 2021/src/runners/day05.rs
@@ -1,11 +1,13 @@
 use advent_of_code::util::*;
-// use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::iter;
 use std::num::ParseIntError;
 use std::str::FromStr;
 
 pub fn run_a() {
-  let lines = file_to_lines("data/test.txt");
-  // let mut _map = HashMap::new();
+  let lines = file_to_lines("data/05_input.txt");
+  let mut map: HashMap<Point, u32> = HashMap::new();
 
   lines
     .map(|line| {
@@ -14,17 +16,69 @@ pub fn run_a() {
         .map(|val| val.trim().parse::<Point>().unwrap())
         .collect::<Vec<_>>()
     })
-    .for_each(|points| println!("{:?}", points))
+    .for_each(|points| update_map(&mut map, &points[0], &points[1]));
+
+  let amt = map.iter().filter(|(_, v)| **v >= 2).fold(0, |mut acc, _| {
+    acc += 1;
+    acc
+  });
+  println!("total: {}", amt);
 }
 
 pub fn run_b() {
   println!("Hello day05, part b!")
 }
 
-#[derive(Debug)]
+fn update_map(map: &mut HashMap<Point, u32>, p1: &Point, p2: &Point) {
+  if p1.x == p2.x {
+    match p1.y.cmp(&p2.y) {
+      Ordering::Less => add_line(Orientation::Horizontal, map, p1, p2),
+      Ordering::Greater => add_line(Orientation::Horizontal, map, p2, p1),
+      Ordering::Equal => add_point(map, p1),
+    }
+  } else if p1.y == p2.y {
+    match p1.x.cmp(&p2.x) {
+      Ordering::Less => add_line(Orientation::Vertical, map, p1, p2),
+      Ordering::Greater => add_line(Orientation::Vertical, map, p2, p1),
+      Ordering::Equal => add_point(map, p1),
+    }
+  }
+}
+
+fn add_line(orient: Orientation, map: &mut HashMap<Point, u32>, p1: &Point, p2: &Point) {
+  let xs: Vec<u32>;
+  let ys: Vec<u32>;
+
+  match orient {
+    Orientation::Vertical => {
+      xs = (p1.x..p2.x + 1).collect();
+      ys = iter::repeat(p1.y).take(xs.len()).collect();
+    }
+    Orientation::Horizontal => {
+      ys = (p1.y..p2.y + 1).collect();
+      xs = iter::repeat(p1.x).take(ys.len()).collect();
+    }
+  }
+
+  for (x, y) in xs.iter().zip(ys) {
+    add_point(map, &Point { x: *x, y });
+  }
+}
+
+fn add_point(map: &mut HashMap<Point, u32>, p: &Point) {
+  let amt = map.entry(*p).or_insert(0);
+  *amt += 1;
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 struct Point {
   x: u32,
   y: u32,
+}
+
+enum Orientation {
+  Vertical,
+  Horizontal,
 }
 
 impl FromStr for Point {

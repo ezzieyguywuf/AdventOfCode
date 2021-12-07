f52fa7f7f4e4173531c5c68a036ced3dea2eaf1d
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-07 12:32:21 -0500

    day06: refactor to be more efficient
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners/day06.rs
+++ 2021/src/runners/day06.rs
@@ -1,46 +1,70 @@
 use advent_of_code::util::*;
+use std::collections::HashMap;
 
 pub fn run_a() {
-  let mut vals = parse();
-  run_simulation(80, &mut vals);
-  println!("day06a: ans = {}", vals.len());
+  let mut data = parse();
+  // println!("parsed");
+  // for i in 0..9 {
+  //   let val = data.get(&i).unwrap_or(&0);
+  //   println!("{}: {}", i, val);
+  // }
+  run_simulation(80, &mut data);
+
+  // println!("\nsimulated");
+  // for i in 0..9 {
+  //   let val = data.get(&i).unwrap_or(&0);
+  //   println!("{}: {}", i, val);
+  // }
+  println!("day06a: ans = {}", data.values().sum::<u32>());
 }
 
 pub fn run_b() {
-  println!("Hello, day06 part b!")
+  // let mut vals = parse();
+  // run_simulation(10, &mut vals);
+  // println!("day06b: ans = {}", vals.len());
+  println!("day06b: hello!")
 }
 
-fn parse() -> Vec<u32> {
-  let lines = file_to_lines("data/06_input.txt");
-  lines
-    .collect::<String>()
-    .split(',')
-    .map(|val| {
-      val
-        .parse::<u32>()
-        .unwrap_or_else(|_| panic!("Unable to parse {:?} into u32", val))
-    })
-    .collect()
+fn parse() -> HashMap<u32, u32> {
+  let line = file_to_lines("data/test.txt").collect::<String>();
+  let vals = line.split(',').map(|val| {
+    val
+      .parse::<u32>()
+      .unwrap_or_else(|_| panic!("Unable to parse {:?} into u32", val))
+  });
+
+  let mut map = HashMap::new();
+  for val in vals {
+    let amt = map.entry(val).or_insert(0);
+    *amt += 1;
+  }
+
+  map
 }
 
-fn run_simulation(days: u32, data: &mut Vec<u32>) {
+fn run_simulation(days: u32, data: &mut HashMap<u32, u32>) {
   for _ in 0..days {
-    let mut new_vals = tick(data);
-    data.append(&mut new_vals);
+    tick(data);
   }
 }
 
-fn tick(data: &mut Vec<u32>) -> Vec<u32> {
-  let mut out = Vec::new();
-  for i in 0..data.len() {
-    let val = &mut data[i];
-    if *val == 0 {
-      *val = 6;
-      out.push(8);
+fn tick(data: &mut HashMap<u32, u32>) {
+  let mut prev: u32 = 0;
+  for day in (0..9).rev() {
+    if day > 0 {
+      let today = data.entry(day).or_insert(0);
+      std::mem::swap(&mut *today, &mut prev);
     } else {
-      *val -= 1;
+      let tmp;
+      {
+        let today = data.entry(day).or_insert(0);
+        tmp = *today;
+        *today = prev;
+      }
+      *data.entry(6).or_insert(0) += tmp;
+      if tmp > 0 {
+        *data.entry(8).or_insert(1) += tmp;
+      }
     }
   }
-
-  out
 }

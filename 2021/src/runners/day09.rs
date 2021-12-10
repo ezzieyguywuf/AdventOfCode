ad9296b467b6f4fdaecb2898f57463a0a2d2d634
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-12-10 14:54:40 -0500

    day09: update graph nodes with incoming edges
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- 2021/src/runners/day09.rs
+++ 2021/src/runners/day09.rs
@@ -14,17 +14,76 @@ pub fn run_a() {
   for row in 0..matrix.rows {
     for col in 0..matrix.cols {
       let coord = Coord { row, col };
+      // these two should always be valid, thus unwrap is ok
       let val = matrix.get_cell(&coord).unwrap();
-      let right = matrix.get_cell(&Coord { row, col: col + 1 });
-      let down = matrix.get_cell(&Coord { row: row + 1, col });
-
-      // this should always be valid, thus unwrap is ok
       let graph_index = matrix.make_index(&coord).unwrap();
-      graph[graph_index].value = *val;
+      {
+        let node: &mut Node = graph.get_mut(graph_index).unwrap();
+        node.value = *val;
+      }
 
-      print!("{}", val);
+      // these two may be invalid
+      let right_coord = Coord { row, col: col + 1 };
+      let down_coord = Coord { row: row + 1, col };
+      let right = matrix.get_cell(&right_coord);
+      let down = matrix.get_cell(&down_coord);
+
+      // for the first and last rows , count the upper and lower (respectively)
+      // walls as incoming
+      if row == 0 || row == matrix.rows - 1 {
+        let node: &mut Node = graph.get_mut(graph_index).unwrap();
+        node.incoming.push(Coord {
+          row: matrix.rows,
+          col: matrix.cols,
+        });
+      }
+
+      // for the first and last columns, count the left and right (respectively)
+      // walls as incoming
+      if col == 0 || col == matrix.cols - 1 {
+        let node: &mut Node = graph.get_mut(graph_index).unwrap();
+        node.incoming.push(Coord {
+          row: matrix.rows,
+          col: matrix.cols,
+        });
+      }
+
+      if let Some(right_val) = right {
+        if val < right_val {
+          let node: &mut Node = graph.get_mut(graph_index).unwrap();
+          node.incoming.push(right_coord);
+        } else if val > right_val {
+          // unwrap should be safe since right_val was Some
+          let right_index = matrix.make_index(&right_coord).unwrap();
+          graph
+            .get_mut(right_index)
+            .unwrap()
+            .incoming
+            .push(coord.clone());
+        }
+      }
+
+      if let Some(down_val) = down {
+        let node: &mut Node = graph.get_mut(graph_index).unwrap();
+        if val < down_val {
+          node.incoming.push(down_coord);
+        } else if val > down_val {
+          // unwrap should be safe since right_val was Some
+          let down_index = matrix.make_index(&down_coord).unwrap();
+          graph
+            .get_mut(down_index)
+            .unwrap()
+            .incoming
+            .push(coord.clone());
+        }
+      }
     }
-    println!();
+  }
+
+  for node in graph {
+    if node.incoming.len() == 4 {
+      println!("node with val {} is local minimum", node.value);
+    }
   }
   println!("day09a: ans = {}", 42);
 }
@@ -66,16 +125,19 @@ fn parse() -> Matrix {
   Matrix { cols, rows, data }
 }
 
+#[derive(Debug, Clone, Copy)]
 struct Coord {
   row: usize,
   col: usize,
 }
 
+#[derive(Debug)]
 struct Node {
   value: u32,
   incoming: Vec<Coord>,
 }
 
+#[derive(Debug)]
 struct Matrix {
   cols: usize,
   rows: usize,

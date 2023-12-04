4787479781cf08782cd1f3f496cc23d21c13f797
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2023-12-03 23:10:44 -0500

    Get rid of unwrap.

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -2,16 +2,22 @@ use std::env;
 
 static DAYS: [i32; 1] = [1];
 
-fn get_arg(target: &str, args: &mut Vec<String>) -> Option<String> {
+fn get_arg(target: &str, args: &mut Vec<String>) -> Result<String, Error> {
     let mut target_arg: String = String::from("--");
     target_arg.push_str(target);
 
     args.iter()
         .position(|arg| arg.starts_with(&target_arg))
-        .map(|i| {
-            let arg = args.remove(i);
-            arg.trim_start_matches(&target_arg).to_string()
-        })
+        .map_or_else(
+            || {
+                println!("Please provide a --{target} argument");
+                Err(Error::MissingArgument)
+            },
+            |i| {
+                let arg = args.remove(i);
+                Ok(arg.trim_start_matches(&target_arg).to_string())
+            },
+        )
 }
 
 #[derive(Debug)]
@@ -23,10 +29,12 @@ enum Error {
 fn main() -> Result<(), Error> {
     let mut args: Vec<String> = env::args().collect();
     let _program_name = args.remove(0);
-    let which_days: Vec<i32> = match args.iter().position(|arg| arg.starts_with("--day")) {
-        Some(i) => {
-            let arg = get_arg("day", &mut args).unwrap();
-            let days: String = if arg.chars().next().unwrap() == '=' {
+    let which_days: Vec<i32> = args
+        .iter()
+        .position(|arg| arg.starts_with("--day"))
+        .map_or(Ok(DAYS.to_vec()), |i| {
+            let arg = get_arg("day", &mut args)?;
+            let days: String = if arg.starts_with('=') {
                 arg.trim_start_matches('=').to_string()
             } else {
                 if args.len() <= i {
@@ -38,12 +46,13 @@ fn main() -> Result<(), Error> {
 
             let mut parsed_days: Vec<i32> = Vec::new();
             for day_string in days.split(',') {
-                let maybe_day = day_string.parse::<i32>();
-                if maybe_day.is_err() {
-                    println!("Could not parse {day_string} into an integer.");
-                    return Err(Error::InvalidArgument);
+                let day = match day_string.parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("Could not parse {day_string} into an integer.");
+                        return Err(Error::InvalidArgument);
+                    }
                 };
-                let day = maybe_day.unwrap();
                 if !DAYS.contains(&day) {
                     println!("Day must be one of {DAYS:?}. Got {day}.");
                     return Err(Error::InvalidArgument);
@@ -51,19 +60,13 @@ fn main() -> Result<(), Error> {
                 parsed_days.push(day);
             }
 
-            parsed_days
-        }
-        None => DAYS.to_vec(),
-    };
+            Ok(parsed_days)
+        })?;
 
     for day in which_days {
         match day {
             1 => {
-                let fname = get_arg("day01_data", &mut args);
-                if fname.is_none() {
-                    println!("Please provide a --day01_data flag");
-                    return Err(Error::MissingArgument);
-                };
+                let fname = get_arg("day01_data", &mut args)?;
                 println!("Got fname: {fname:?}");
             }
             n => println!("Sorry, don't know what to do with day '{n}' yet'"),

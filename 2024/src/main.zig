5aaf485f738946cc6d046df6b7cce21befd7cc9b
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2024-12-02 18:54:15 -0500

    Add --filename flag and file parsing.

--- 2024/src/main.zig
+++ 2024/src/main.zig
@@ -7,13 +7,7 @@ pub fn main() !u8 {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("Welcome to Advent of Code, 2024 edition. Wolfgang E. Sanyer did this.\n", .{});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-    try stdout.print("Run `zig build run -- --help` to (maybe?) see documentation\n", .{});
+    std.debug.print("Run `zig build run -- --help` to (maybe?) see documentation\n", .{});
 
     // Apparently arena.deinit() is easy? Chosen based on https://ziglang.org/documentation/master/#Choosing-an-Allocator
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -21,30 +15,30 @@ pub fn main() !u8 {
 
     const allocator = arena.allocator();
 
-    const problem = process_args(allocator) catch return 1;
-    try stdout.print("Got problem for day {d}\n", .{problem.day});
+    const problem = processArgs(allocator) catch return 1;
 
-    try bw.flush(); // Don't forget to flush!
+    std.debug.print("Got problem for day {d}\n", .{problem.day});
+    std.debug.print("Got data: (see below)\n", .{});
+    for (problem.data) |line| {
+        std.debug.print("{s}\n", .{line});
+    }
 
     return 0;
 }
 
-fn process_args(allocator: std.mem.Allocator) ArgParseError!Problem {
+fn processArgs(allocator: std.mem.Allocator) !Problem {
     var args = try std.process.argsWithAllocator(allocator);
     // Is this strictly necessary? does arena.deinit() take care of it?
     defer args.deinit();
 
     var problem: Problem = Problem{
         .day = 0,
-        .data = "",
+        .data = &.{},
     };
     while (args.next()) |arg| {
         // try out.print("Got arg {s}\n", .{arg});
         if (std.mem.eql(u8, arg, "--day") or std.mem.eql(u8, arg, "-d")) {
-            const day_str = args.next() orelse {
-                std.debug.print("When using --day or -d, please specify which day to solve for after, e.g. `--day 1`\n", .{});
-                return ArgParseError.MissingArgument;
-            };
+            const day_str = try nextArg("--day or -d", &args);
             const day = std.fmt.parseInt(u8, day_str, 10) catch |err| switch (err) {
                 error.InvalidCharacter => {
                     std.debug.print("Unable to convert '{s}' into an integer\n", .{day_str});
@@ -61,6 +55,11 @@ fn process_args(allocator: std.mem.Allocator) ArgParseError!Problem {
             }
             problem.day = day;
         }
+
+        if (std.mem.eql(u8, arg, "--filename") or std.mem.eql(u8, arg, "-f")) {
+            const filename = try nextArg("--filename or -f", &args);
+            problem.data = try readFile(allocator, filename);
+        }
     }
 
     if (problem.day == 0) {
@@ -71,12 +70,55 @@ fn process_args(allocator: std.mem.Allocator) ArgParseError!Problem {
     return problem;
 }
 
+// Caller is responsible for deinit'ing the lines ArrayList
+fn readFile(allocator: std.mem.Allocator, filename: string) !lines {
+    var dir = std.fs.cwd();
+    defer dir.close();
+    var targetFile = filename;
+
+    if (std.fs.path.isAbsolute(targetFile)) {
+        const dirname = std.fs.path.dirname(targetFile) orelse {
+            std.debug.print("'{s}' looks like an absolute path, but cannot parse the dirname", .{targetFile});
+            return FileReadError.CannotParseDirname;
+        };
+        targetFile = std.fs.path.basename(targetFile);
+
+        dir = try std.fs.openDirAbsolute(dirname, .{});
+    }
+
+    const file = try dir.openFile(targetFile, .{});
+    defer file.close();
+
+    // stolen from https://stackoverflow.com/a/68879352
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var buf: [1024]u8 = undefined;
+    var out = std.ArrayList(string).init(allocator);
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try out.append(try allocator.dupe(u8, line));
+    }
+
+    return out.toOwnedSlice();
+}
+fn nextArg(flagname: string, args: *std.process.ArgIterator) ArgParseError!string {
+    return args.next() orelse {
+        std.debug.print("Missing argument after flag {s}\n", .{flagname});
+        return ArgParseError.MissingArgument;
+    };
+}
+
 const Problem = struct {
     day: u8,
-    data: []const u8,
+    data: lines,
 };
 
 const ArgParseError = error{
     MissingArgument,
     InvalidArgument,
 };
+
+const FileReadError = error{CannotParseDirname};
+
+const string = []const u8;
+const lines = []string;
